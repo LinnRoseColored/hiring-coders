f55551d2e67ee37b5dafadc7b7e364aee15372f5
Aline Braga <alinesssbraga@hotmail.com>
2021-07-09 15:44:42 -0300

    continuando exercicios

--- JavaScript Básico/Exercícios/exercicio2.js
+++ JavaScript Básico/Exercícios/exercicio2.js
@@ -1,1 +1,8 @@
-// Reverse a string
+// Conversor de Celsius para Fahrenheit
+
+function convertToFahrenheit(value) {
+    return value * 1.8 + 32
+}
+
+let result = convertToFahrenheit(40)
+console.log('A temperatura em F é ${result}')
