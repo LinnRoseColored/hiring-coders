f3de8c1ba77435a15fc94fd4ab9ac275e1834291
Aline Braga <alinesssbraga@hotmail.com>
2021-07-08 22:32:11 -0300

    consegui faze esse cacete funciona

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,31 +1,6 @@
-console.log("Hello World");
+console.log("hello world!")
 
-//Tipos de variáveis em JS
-// var; let; const
 
-var myName = "aline" // string (variável de texto)
 
-var age = "25" // number inclusive decimal
-
-var isStudent = false //boolean (tipo lógico)
-
-var person = {
-    "name": "aline"
-    "age": 25
-}
-// objeto, conjunto de dados de qualquer tipo, chave (nome) smp é string 
-
-var students = [Aline, Vini, Guibs]
-//array é uma lista, vários dados na mesma linha
-
-function sum(a+b) {
-    return a + b
-}
-//Funções são objetos que podem ser "chamados" ou dão um retorno
-
-// operadores aritiméticos
-
-var soma = 1 + 3
-console.log(soma)
 
 
